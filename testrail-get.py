9f7fdf8106bc8387722da3c65ff17c8448783aec
Aaron Train <aaron.train@gmail.com>
2020-11-30 15:20:35 -0500

    print all custom automation statuses
    minor cleanup
    
    minor cleanup

--- testrail-get.py
+++ testrail-get.py
@@ -1,5 +1,6 @@
 import os, json
 from pprint import pprint
+from enum import Enum
 from testrail import *
 
 client = APIClient('https://testrail.stage.mozaws.net')
@@ -15,10 +16,14 @@ print("Fetching project data from Testrail..." + "\n")
 
 projects = client.send_get('get_projects')
 
-untriaged, suitable, unsuitable, completed, disabled = range(1, 6)
+class Status(Enum):
+    UNTRIAGED = 1
+    SUITABLE = 2
+    UNSUITABLE = 3
+    COMPLETED = 4
+    DISABLED = 5
 
-automation_disabled = []
-automation_suitable = []
+automation_untriaged, automation_suitable, automation_unsuitable, automation_completed, automation_disabled = ([] for i in range(5))
 
 for project in projects:
     project_id = project['id']
@@ -28,9 +33,21 @@ for project in projects:
            suite_id = suite['id']
            cases = client.send_get('get_cases/%s&suite_id=%s' % (project_id, suite_id))
            for case in cases:
-               if case['custom_automation_status'] == disabled:
-                   automation_disabled.append(case['title'])
-               elif case['custom_automation_status'] == suitable:
-                   automation_suitable.append(case['title'])
+               if case['custom_automation_status'] == Status.DISABLED.value:
+                   automation_disabled.append(case)
+               elif case['custom_automation_status'] == Status.SUITABLE.value:
+                   automation_suitable.append(case)
+               elif case['custom_automation_status'] == Status.UNSUITABLE.value:
+                   automation_unsuitable.append(case)
+               elif case['custom_automation_status'] == Status.UNTRIAGED.value:
+                   automation_untriaged.append(case)
+               elif case['custom_automation_status'] == Status.COMPLETED.value:
+                   automation_completed.append(case)
+               else: 
+                   pass
 
 print("{} disabled automated tests.".format(str(len(automation_disabled))))
+print("{} unsuitable automated tests.".format(str(len(automation_unsuitable))))
+print("{} untriaged automated tests.".format(str(len(automation_untriaged))))
+print("{} suitable automated tests.".format(str(len(automation_suitable))))
+print("{} completed automated tests.".format(str(len(automation_completed))))
